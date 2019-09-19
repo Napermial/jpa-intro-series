1777d51276582624d9b2883ddbe2a366ef2484dd
Napermial <h>
2019-09-19 20:04:30 +0200

    init

---
+++ src/main/java/com/codecool/jpaintroproject/JpaIntroProjectApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.jpaintroproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaIntroProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaIntroProjectApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/codecool/jpaintroproject/entities/Episode.java
@@ -0,0 +1,4 @@
+package com.codecool.jpaintroproject.entities;
+
+public class Episode {
+}
